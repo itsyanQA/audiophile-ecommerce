8f3d7785a4c2a82577815656ed8c096420ceaca3
stron <yaneliyahu83@gmail.com>
2024-03-02 17:51:19 +0200

    feat(custom-hook): create a useDeviceEvaluator custom hook
    returns a string union of the following: mobile, tablet or desktop. used for selecting correct images by device from the data.json attributes

---
+++ src/hooks/use-device-evaluator.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export function useDeviceEvaluator() {
+  const [width, setWidth] = useState<number>(window.innerWidth);
+
+  const resizeEventHandler = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", resizeEventHandler);
+
+    return () => {
+      window.removeEventListener("resize", resizeEventHandler);
+    };
+  }, []);
+
+  const getDeviceType = (): "mobile" | "tablet" | "desktop" => {
+    if (width <= 600) {
+      return "mobile";
+    } else if (width > 600 && width < 900) {
+      return "tablet";
+    }
+
+    return "desktop";
+  };
+
+  return getDeviceType();
+}
