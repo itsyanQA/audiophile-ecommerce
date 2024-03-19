4a2782abf531974c6901d3578c9cb5a1c92ea0cc
stron <yaneliyahu83@gmail.com>
2024-03-19 19:46:15 +0200

    feat(button): create button component with variants

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,10 +1,18 @@
-import { PropsWithChildren } from "react";
+import { ButtonHTMLAttributes, HTMLAttributes, PropsWithChildren } from "react";
 import "./button.scss";
 
 type ButtonProps = {
   variant: "colorful" | "transparent" | "black";
-} & PropsWithChildren;
+  fullWidth?: boolean;
+} & PropsWithChildren &
+  ButtonHTMLAttributes<HTMLButtonElement>;
 
-export function Button({ variant, children }: ButtonProps) {
-  return <button className={`button button--${variant}`}>{children}</button>;
+export function Button(props: ButtonProps) {
+  const { variant, fullWidth, children, ...rest } = props;
+
+  return (
+    <button className={`button button--${variant} ${fullWidth ? "button--full-width" : ""}`} {...rest}>
+      {children}
+    </button>
+  );
 }
