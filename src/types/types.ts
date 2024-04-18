91789212e923bde2187705b459563ced3c65ee96
stron <yaneliyahu83@gmail.com>
2024-04-18 22:40:27 +0300

    feat(types): add zod formScheme and more types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 type Image = {
   mobile: string;
   tablet: string;
@@ -24,6 +26,12 @@ export type Cart = {
   id: number;
 };
 
+export type CartPrices = {
+  totalItemPrice: number;
+  vat: number;
+  grandTotal: number;
+};
+
 export type IncludedItem = {
   quantity: number;
   item: string;
@@ -46,3 +54,31 @@ export type Product = {
 };
 
 export type Category = "HEADPHONES" | "SPEAKERS" | "EARPHONES";
+
+export type TPaymentMethod = "e-Money" | "Cash on Delivery";
+
+export const formScheme = z.object({
+  name: z.string().min(1, "Name is required"),
+  email: z.string().email(),
+  phone: z.number({ invalid_type_error: "Invalid phone" }).gte(1000000, "Number is too short").lte(999999999999999, "Number is too long"),
+  address: z.string({ invalid_type_error: "Invalid address" }).min(5, "Invalid address"),
+  zipCode: z.string({ invalid_type_error: "Invalid zip-code" }).min(5, "Zip-code too short"),
+  city: z.string().min(1, "Field is required"),
+  country: z.string().min(4, "Country name is too short"),
+  eMoneyNumber: z
+    .number({ invalid_type_error: "Invalid money number" })
+    .gte(100000000, "Number must have 9 digits")
+    .lte(999999999, "Number must have 9 digits")
+    .optional(),
+  eMoneyPin: z
+    .number({ invalid_type_error: "Invalid pin" })
+    .gte(1000, "Pin must have 4 digits")
+    .lte(9999, "Pin must have 4 digits")
+    .optional(),
+});
+
+export type TFormScheme = z.infer<typeof formScheme>;
+
+export type Page = "/" | "/headphones" | "/speakers" | "/earphones" | "/product" | "/checkout";
+
+export type Device = "mobile" | "tablet" | "desktop";
